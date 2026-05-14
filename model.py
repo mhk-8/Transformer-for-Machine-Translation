fbe07ceac41d2fd06ab6439437a2ac648b3c066e
Hari Krishna <majjiharikrishna2002@gmail.com>
2026-05-14 14:38:08 +0530

    400 epochs file id updated

--- model.py
+++ model.py
@@ -520,7 +520,7 @@ class Transformer(nn.Module):
         _ckpt = None
         if checkpoint_path is not None:
             if not os.path.exists(checkpoint_path):
-                gdown.download(id="1hmC24_xv3dgKZZkm6RWeSwyUY4jGytSk", output=checkpoint_path, quiet=False)
+                gdown.download(id="1a2QOZ_wc5r_Q6CUsv5AyaAOpRGRODwoe", output=checkpoint_path, quiet=False)
             _ckpt = torch.load(checkpoint_path, map_location='cpu')
             if 'model_config' in _ckpt:
                 _cfg = _ckpt['model_config']
